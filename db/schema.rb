33c25596d5e3b4ec662127e612b1edab3d660c5e
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2021-03-10 08:02:12 +0100

    add message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_100313) do
+ActiveRecord::Schema.define(version: 2021_03_10_064325) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
